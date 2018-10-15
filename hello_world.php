d771392b84ec6b923e828a63a39d12116a7823ea
Termsaithong <Panat.Termsaithong@emc.com>
2018-10-15 14:44:38 +0700

    1st change in 1stbranch

---
+++ hello_world.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+<meta charset="UTF-8">
+ <title><?php print ("Hello!! PHP"); ?></title>
+ </head>
+
+ <body>
+	<?php
+		print ("Hello World");
+		$name = false;
+	?>
+ </body>
+
+ </html> 
